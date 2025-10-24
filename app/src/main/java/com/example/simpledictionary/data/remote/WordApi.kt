c641d90851c07f0c8f176e5b6823ff99dcc6c031
Amanuel Moha <mohaaman922@gmail.com>
2025-10-24 17:59:34 +0400

    api return type

--- app/src/main/java/com/example/simpledictionary/data/remote/WordApi.kt
+++ app/src/main/java/com/example/simpledictionary/data/remote/WordApi.kt
@@ -1,8 +1,9 @@
 package com.example.simpledictionary.data.remote
 
+import com.example.simpledictionary.data.remote.dto.WordDetailDto
 import retrofit2.http.GET
 
 interface WordApi {
     @GET("/api/v1")
-    suspend fun getWordDetail()
+    suspend fun getWordDetail(): List<WordDetailDto>
 }
