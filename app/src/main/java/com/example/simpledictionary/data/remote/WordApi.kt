d5c407bb843fcbda173f65a508f1187c8198c60a
Amanuel Moha <mohaaman922@gmail.com>
2025-10-25 16:56:46 +0400

    api path variables

--- app/src/main/java/com/example/simpledictionary/data/remote/WordApi.kt
+++ app/src/main/java/com/example/simpledictionary/data/remote/WordApi.kt
@@ -2,8 +2,9 @@ package com.example.simpledictionary.data.remote
 
 import com.example.simpledictionary.data.remote.dto.WordDetailDto
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface WordApi {
-    @GET("/api/v1")
-    suspend fun getWordDetail(): List<WordDetailDto>
+    @GET("/api/v1/entries/{lang}/{word}")
+    suspend fun getWordDetail(@Path("word") word:String,@Path("lang") lang:String="en"): List<WordDetailDto>
 }
