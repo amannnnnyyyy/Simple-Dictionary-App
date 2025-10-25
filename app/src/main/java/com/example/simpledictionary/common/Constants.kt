dccd358ecdb74acb77abce19a07363e517300b55
Amanuel Moha <mohaaman922@gmail.com>
2025-10-25 17:29:15 +0400

    dagger hilt word api injection

--- app/src/main/java/com/example/simpledictionary/common/Constants.kt
+++ app/src/main/java/com/example/simpledictionary/common/Constants.kt
@@ -1,5 +1,5 @@
 package com.example.simpledictionary.common
 
 object Constants {
-
+    const val BASE_URL = "https://freedictionaryapi.com"
 }
---
+++ app/src/main/java/com/example/simpledictionary/di/AppModule.kt
@@ -0,0 +1,26 @@
+package com.example.simpledictionary.di
+
+import com.example.simpledictionary.common.Constants.BASE_URL
+import com.example.simpledictionary.data.remote.WordApi
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun providesWordDetailAPi(): WordApi{
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(WordApi::class.java)
+    }
+}
